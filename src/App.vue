7a227ecf1f0a09ca35c6b35ba2f57ea9926dc9b6
lixin <lixin544@163.com>
2020-09-07 22:40:54 +0800

    add Quasar
    new ui

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,12 @@
-
 <template>
-  <v-app id="blog">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
-      <v-img
-        :aspect-ratio="16/9"
-        src="https://images.freeimages.com/images/small-previews/535/natural-wonders-1400924.jpg"
-      >
-        <v-row
-          align="end"
-          class="lightbox white--text pa-2 fill-height"
-        >
-          <v-col>
-            <div class="subheading">
-              blog
-            </div>
-          </v-col>
-        </v-row>
-      </v-img>
-      <v-list dense>
-        <v-list-item
-          link
-          to="/"
-        >
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-              主页
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item
-          link
-          to="/manage/article/list"
-        >
-          <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-              文章管理
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-content>
-      <v-container>
-        <router-view />
-      </v-container>
-    </v-content>
-  </v-app>
+  <div id="q-app">
+    <router-view />
+  </div>
 </template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
 
-<script>
-  export default {
-    data: () => ({
-      drawer: null,
-    }),
-    created () {
-      this.$vuetify.theme.dark = false
-    },
-    methods: {
-      navTo (to) {
-        this.$router.push(to)
-      },
-    },
-  }
+export default defineComponent({
+  name: 'App'
+});
 </script>
-
-<style lang="less">
- .theme--light.v-application{
-   background: rgba(0, 0, 0, 0.001) !important;
-   code{
-     box-shadow: none;
-   }
- }
-</style>
