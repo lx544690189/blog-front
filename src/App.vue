66f08743c3b805a5f02e5b960bbe0505f7f5cae9
lixin <lixin544@163.com>
2020-09-18 00:08:23 +0800

    feat add home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="q-app">
-    <router-view />
-  </div>
+<div id="q-app">
+  <router-view />
+</div>
 </template>
+
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import {
+  defineComponent,
+} from '@vue/composition-api';
 
 export default defineComponent({
   name: 'App',
 });
 </script>
+
+<style lang="scss">
+#q-app {
+  position: relative;
+  height: 100vh;
+}
+</style>
--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,22 +1,43 @@
 <template>
 <div>
-  <div class="card-user">
-
+  <div class="slider-content">
+    <div class="avatar">
+      <img :src="avatarImg" />
+    </div>
+    <div class="user-info">
+      <div class="name">星途</div>
+      <div class="desc">web developer</div>
+      <div class="github-follow-btn"><i class="iconfont icon-github"></i> github</div>
+    </div>
+    <div class="menu">
+      <div class="menu-item active">首页</div>
+      <div class="menu-item">作品</div>
+      <div class="menu-item">归档</div>
+    </div>
   </div>
+  <div class="main-content"></div>
 </div>
 </template>
 
 <script lang="ts">
+import avatarImg from '../assets/avatar.jpeg';
+console.log('avatarImg: ', avatarImg);
 import {
   defineComponent,
 } from '@vue/composition-api';
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    return {
+      avatarImg
+    }
+  }
+});
 </script>
 
 <style lang="scss">
 body {
-  background-color: #e6e7ee;
+  background-color: #2F3138;
   min-height: 100vh;
 }
 
@@ -29,5 +50,115 @@ body {
   border-color: #d1d9e6;
   background: linear-gradient(145deg, #fefefe, #d5d5d5);
   box-shadow: 3px 3px 6px #b8b9be, -3px -3px 6px #fff;
+}
+
+.slider-content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 300px;
+  bottom: 0;
+  padding-top: 30px;
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    margin: 0 auto;
+    border-radius: 50%;
+    overflow: hidden;
+    padding: 8px;
+    background-color: #c5c0c0;
+    // box-shadow: 8px 8px 6px rgba(255, 255, 255, .5), -8px -8px 16px rgba(0, 0, 0, .2), -8px 8px 16px rgba(0, 0, 0, .2), 8px -8px 16px rgba(0, 0, 0, .2);
+    box-shadow: inset 5px 5px 28px #070808,
+      inset -9px -9px 38px #070808, 5px 5px 21px #2c2e31,
+      -5px -5px 21px #3e4140;
+
+    img {
+      width: 100%;
+      opacity: .7;
+      border-radius: 50%;
+    }
+  }
+
+  .user-info {
+    padding: 10px 0;
+    text-align: center;
+
+    .name {
+      text-align: center;
+      font-size: 16px;
+      color: #fdfdfd;
+      font-weight: bold;
+    }
+
+    .desc {
+      text-align: center;
+      font-size: 12px;
+      color: #dfdcd9;
+    }
+  }
+
+  .github-follow-btn {
+    transform: scale(.9);
+    display: inline-block;
+    height: 30px;
+    line-height: 30px;
+    padding: 0 10px;
+    text-align: center;
+    color: #fefefe;
+    background: #454547;
+    border-radius: 6px;
+    font-size: 12px;
+    margin-top: 10px;
+    cursor: pointer;
+    border: 1px solid #696a6d;
+    box-shadow: 6px 6px 48px #1f2025,
+      -6px -6px 48px #3f424b;
+    background: linear-gradient(145deg, #2a2c32, #32343c);
+    box-shadow: 6px 6px 13px #18191c,
+      -6px -6px 13px #3e4046;
+
+    .icon-github {
+      float: left;
+      margin-right: 6px;
+    }
+  }
+
+  .menu {
+    margin-top: 20px;
+    padding: 20px 0;
+    color: #c5c0c0;
+
+    &-item {
+      cursor: pointer;
+      margin: 20px auto;
+      height: 30px;
+      width: 160px;
+      line-height: 30px;
+      text-align: center;
+      border-radius: 20px;
+
+      background: linear-gradient(145deg, #2a2c32, #32343c);
+      box-shadow: 6px 6px 13px #18191c, -4px -3px 8px #36383e;
+
+      &.active {
+        background: #403540;
+        box-shadow: inset 4px 4px 4px 0px #24252b, inset 0px -1px 0px #3a3d45;
+
+      }
+    }
+  }
+}
+
+.main-content {
+  position: absolute;
+  top: 0;
+  left: 300px;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  box-shadow: 0 5px 6px -3px rgba(0, 0, 0, 0.5), 0 9px 12px 1px rgba(0, 0, 0, 0.6), 0 3px 16px 2px rgba(0, 0, 0, 0.5);
 }
 </style>
